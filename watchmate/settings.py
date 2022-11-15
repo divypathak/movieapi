54d767434507ac2501ce22f5c3cc6ce3521dfba0
divypathak <divypathak@gmail.com>
2022-11-16 03:19:29 +0530

    adfaf

--- watchmate/settings.py
+++ watchmate/settings.py
@@ -141,15 +141,9 @@ SIMPLE_JWT = {
     'ROTATE_REFRESH_TOKENS' : True
 }
 
-CORS_REPLACE_HTTPS_REFERER      = False
-HOST_SCHEME                     = "http://"
-SECURE_PROXY_SSL_HEADER         = None
-SECURE_SSL_REDIRECT             = False
-SESSION_COOKIE_SECURE           = False
-CSRF_COOKIE_SECURE              = False
-SECURE_HSTS_SECONDS             = None
-SECURE_HSTS_INCLUDE_SUBDOMAINS  = False
-SECURE_FRAME_DENY               = False
+SECURE_SSL_REDIRECT=False
+SESSION_COOKIE_SECURE=False
+CSRF_COOKIE_SECURE=False
 
 # this rotate refresh tokens : true, helps to generate new refresh token every time we refreshes our access token
 # with the previous access token
